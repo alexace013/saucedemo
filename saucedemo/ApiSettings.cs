272b2daed103662976646a6af287c8588c1e7070
Oleksandr <alexace013@users.noreply.github.com>
2025-01-29 06:32:25 -0800

    Create ApiSettings.cs

---
+++ saucedemo/ApiSettings.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class ApiSettings
+{
+    public string BaseUrl { get; set; }
+    public string Endpoint { get; set; }
+    public string ContentType { get; set; }
+}
+
+public static class ConfigurationHelper
+{
+    public static ApiSettings GetApiSettings()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var apiSettings = config.GetSection("ApiSettings").Get<ApiSettings>();
+        return apiSettings;
+    }
+}
